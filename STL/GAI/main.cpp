71cf75808649b72b82f1aed75273b1da0027288b
Student <Student@304-1>
2019-10-09 21:10:39 +0300

    GAI done

--- STL/GAI/main.cpp
+++ STL/GAI/main.cpp
@@ -3,6 +3,7 @@
 #include <map>
 #include <forward_list>
 #include<Windows.h>
+#include<conio.h>
 
 #include "Crime.h"
 
@@ -10,6 +11,7 @@ using namespace std;
 
 void print(const map<string, forward_list<Crime>>& database);
 void print(const map<string, forward_list<Crime>>& database, const string& id);
+void print(const map<string, forward_list<Crime>>& database, const string& start_id, const string& second_id);
 bool add(map<string, forward_list<Crime>>& database, const string& id, const string& plase, const string& crime);
 
 void main()
@@ -24,12 +26,12 @@ void main()
 
 	do
 	{
-		bool exit = false;
+		cout << "=============================\n\n";
+
 		cout << "1. Вывод всей базы;" << endl;
 		cout << "2. Вывод информации по номеру;" << endl;
 		cout << "3. Вывод информации по диапазону номеров;" << endl;
 		cout << "4. Добавляем записи; " << endl;
-		cout << "5. Выход; " << endl;
 
 		cout << "\n\n=============================\n\n";
 
@@ -38,15 +40,28 @@ void main()
 
 		switch (key)
 		{
-		case '1': print(database); break;
+		case '1': system("CLS"); print(database); break;
 		case '2':
 		{
+			system("CLS");
 			string id;
 			cout << "Введите номер автомобиля: "; cin >> id;
 			print(database, id);
 		}
 		break;
-		case '3': cout << "Данный пункт временно недоступен..." << endl; break;
+		case '3': 
+		{
+			system("CLS");
+			string start_id, second_id;
+			cout << "Введите начальный номер: "; 
+			cin.sync();
+			cin.get(); 
+			getline(cin, start_id);
+			cout << "Введите конечный номер:"; 
+			getline(cin, second_id);
+			print(database, start_id, second_id);
+		}
+		break;
 		case '4':
 		{
 			string id;
@@ -62,10 +77,10 @@ void main()
 			SetConsoleOutputCP(866);
 			cout << "Введите место происшествия: ";
 			SetConsoleCP(1251);
-			SetConsoleOutputCP(1251);
+			//SetConsoleOutputCP(1251);
 			getline(cin, plase);
 			SetConsoleCP(866);
-			SetConsoleOutputCP(866);
+			//SetConsoleOutputCP(866);
 			cout << "Введите описание нарушения: ";
 			SetConsoleCP(1251);
 			SetConsoleOutputCP(1251);
@@ -80,12 +95,12 @@ void main()
 			{
 				cout << "Номер уже есть в базе, добавление происшествия прошло успешно..." << endl;
 			}
-			break;
 		}
-		case '5': exit = true; break;
+		break;
 		}
 		cout << "\n\n=============================\n\n";
-	} while (exit);
+		cout << "Для выхода нажмите ESC";
+	} while (getch()!=27);
 }
 
 void print(const map<string, forward_list<Crime>>& database)
@@ -117,20 +132,51 @@ void print(const map<string, forward_list<Crime>>& database, const string& id)
 	if (!present_in_base) cout << "В базе нет такого номера" << endl;
 }
 
-bool add(map<string, forward_list<Crime>>& database, const string& id, const string& plase, const string& crime)
+void print(const map<string, forward_list<Crime>>& database, const string& start_id, const string& second_id)
 {
 	bool present_in_base = false;
 	for (pair<string, forward_list<Crime>>i : database)
 	{
+		if (i.first >= start_id && i.first <= second_id)
+		{
+			for (Crime j : i.second)
+			{
+				cout << j.get_place() << "\t" << j.get_crime() << ";\n";
+			}
+		}
+	}
+	if (!present_in_base) cout << "В базе нет такого номера" << endl;
+}
+
+bool add(map<string, forward_list<Crime>>& database, const string& id, const string& place, const string& crime)
+{
+	/*for (pair<string, forward_list<Crime>>i : database)
+	{
 		if (id == i.first)
 		{
 			present_in_base = true;
-			i.second.push_front(Crime(plase, crime));
+			i.second.push_front(Crime(place, crime));
+		}
+	}*/
+	//for (map<string, forward_list<Crime>>::iterator it = database.begin(); it != database.end(); it++)
+	//{
+	//	if (id == it->first)
+	//	{
+	//		present_in_base = true;
+	//		it->second.push_front(Crime(place, crime));
+	//	}
+	//}
+	//if (!present_in_base)
+	//{
+	//	database.insert(pair<string, forward_list<Crime>>(id, { Crime(place, crime) }));
+	//}
+	database[id].push_front(Crime(place, crime));
+	for (pair<string, forward_list<Crime>>i : database)
+	{
+		if (id == i.first)
+		{
+			return true;
 		}
 	}
-	if (!present_in_base)
-	{
-		database.insert(pair<string, forward_list<Crime>>(id, { Crime(plase, crime) }));
-	}
-	return present_in_base;
+	return false;
 }
